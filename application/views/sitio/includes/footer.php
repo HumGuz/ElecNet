afcd83eb4ab5cffa140aa538356f6d4b00684c3c
HumGuz <humden_58@hotmail.com>
2018-09-03 11:05:02 -0500

    imagenes

--- application/views/sitio/includes/footer.php
+++ application/views/sitio/includes/footer.php
@@ -19,11 +19,11 @@
             <h5 class="links-title">Quienes Somos<a class="expander visible-xs" href="#TabBlock-1">+</a></h5>
             <div class="tabBlock" id="TabBlock-1">
               <ul class="list-links list-unstyled">
+              	<li><a href="faq.html">Acerca de Elecnet</a></li>
                 <li><a href="#s">Código de Ética</a></li>
                 <li><a href="#">Calidad de los Productos</a></li>
                 <li><a href="sitemap.html">Envíos sin Costo</a></li>
                 <li><a href="#">Alta Fidelidad</a></li>
-                <li><a href="faq.html">Soporte Técnico</a></li>
                 <li><a href="#">Garantías </a></li>
               </ul>
             </div>
@@ -31,30 +31,30 @@
         </div>
         <div class="col-sm-3 col-md-2 col-xs-12 collapsed-block">
           <div class="footer-links">
-            <h5 class="links-title">Insider<a class="expander visible-xs" href="#TabBlock-3">+</a></h5>
+            <h5 class="links-title">Soporte<a class="expander visible-xs" href="#TabBlock-3">+</a></h5>
             <div class="tabBlock" id="TabBlock-3">
               <ul class="list-links list-unstyled">
-                <li><a href="sitemap.html"> Sites Map </a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Trends</a></li>
-                <li><a href="about_us.html">About Us</a></li>
-                <li><a href="contact_us.html">Contact Us</a></li>
-                <li><a href="#">My Orders</a></li>
+                <li><a href="sitemap.html"> Mapa de Tiendas </a></li>
+                <li><a href="#">Soporte Técnico</a></li>
+                <li><a href="#">Centro de Servicio</a></li>
+                <li><a href="about_us.html">Quejas y Sugerencias</a></li>
+                <li><a href="contact_us.html">Contacto</a></li>
+                <li><a href="#">Mis Ordenes</a></li>
               </ul>
             </div>
           </div>
         </div>
         <div class="col-sm-3 col-md-2 col-xs-12 collapsed-block">
           <div class="footer-links">
-            <h5 class="links-title">Service<a class="expander visible-xs" href="#TabBlock-4">+</a></h5>
+            <h5 class="links-title">Servicios <a class="expander visible-xs" href="#TabBlock-4">+</a></h5>
             <div class="tabBlock" id="TabBlock-4">
               <ul class="list-links list-unstyled">
-                <li><a href="account_page.html">Account</a></li>
-                <li><a href="wishlist.html">Wishlist</a></li>
-                <li><a href="shopping_cart.html">Shopping Cart</a></li>
-                <li><a href="#">Return Policy</a></li>
-                <li><a href="#">Special</a></li>
-                <li><a href="#">Lookbook</a></li>
+                <li><a href="account_page.html">Facturación en línea</a></li>
+                <li><a href="wishlist.html">Activar Cuenta</a></li>
+                <li><a href="shopping_cart.html">Recuperar contraseña/a></li>
+                <li><a href="#">¿Quieres Ser Distribuidor?</a></li>
+                <li><a href="#">Bolsa de Trabajo</a></li>
+                <li><a href="#">Calificaciones de Socios</a></li>
               </ul>
             </div>
           </div>
@@ -64,7 +64,7 @@
             <h5 class="links-title">HORAS LABORALES<a class="expander visible-xs" href="#TabBlock-5">+</a></h5>
             <div class="tabBlock" id="TabBlock-5">
               <div class="footer-description">Las órdenes de compra que se procesan en el siguiente horario son enviadas el mismo día.</div>
-              <div class="footer-description"> <b>Lunes-Viernes:</b> 8.00 a.m. - 5.30 p.m.<br>
+              <div class="footehttps://www.zegucom.com.mx/index.php?mod=metodosenvior-description"> <b>Lunes-Viernes:</b> 8.00 a.m. - 5.30 p.m.<br>
                 <b>Sábado:</b> 9.00 a.m. - 2.00 p.m.<br>
                 <b>Domingo:</b> Cerrado </div>
               <div class="payment">
