34f0cc9592da3b9041ba9b32d391877e30888603
HumGuz <humden_58@hotmail.com>
2018-10-02 13:11:39 -0500

    cambios

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+date_default_timezone_set("America/Mexico_City");
+setlocale(LC_TIME, "es_MX.utf8");
 class Usuarios extends CI_Controller {
 	public function index()
 	{
--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -41,9 +41,9 @@ class Login_model extends CI_Model {
 					unset($r['status']);
 					$s['db'] = $r['db_name']; 
 					unset($r['db_name']);
-					// $s['cliente'] = $r;
+					$s['cliente'] = $r;
 					$this->session->set_userdata($s);
-					$_SESSION = $s;					
+					// $_SESSION['userdata'] = $s;
 					return array('code'=>1252);
 				}else{
 					return array('email'=>'Error desconocido');
